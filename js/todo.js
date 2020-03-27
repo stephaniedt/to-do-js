646a28197bec82bac31d66f983f0419543430966
Stephanie Tavares <stephanie.diast@hotmail.com>
2020-03-27 12:39:43 -0300

    primeiro commit

---
+++ js/todo.js
@@ -0,0 +1,38 @@
+let addTarefa = document.querySelector(".addTarefa")
+let container = document.querySelector(".container")
+let inputTextoTarefa = document.querySelector("#textoTarefa")
+
+// addTarefa.addEventListener('click', function(){
+//     alert("Tarefa adicionada à lista (y)")
+// });
+// //funcao como parametro da funcao.
+
+
+addTarefa.onclick = function() {
+    
+    let divCard = document.createElement('div');
+    divCard.setAttribute('class','card text-white bg-info mb-3');
+
+    let divCardBody = document.createElement('div')
+    divCardBody.setAttribute('class','card-body')
+    
+    let textoTarefa = inputTextoTarefa.value
+
+    let pConteudo = document.createElement('p')
+    pConteudo.setAttribute('class','card-text')
+    pConteudo.textContent = textoTarefa;
+    
+    let btnAddTarefa = document.createElement('button')
+    btnAddTarefa.setAttribute('class', 'btn btn-light')
+    btnAddTarefa.textContent = "FinalizarTarefa"
+
+
+    divCard.appendChild(divCardBody)
+    divCardBody.appendChild(pConteudo)
+    divCardBody.appendChild(btnAddTarefa)
+
+    container.appendChild(divCard)
+    inputTextoTarefa.value = ""
+
+}
+//on click: atribuicao do metodo

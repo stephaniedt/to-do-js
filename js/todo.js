61bdd84d72b81dcd2d3a109c43801fa7a81d84e1
Stephanie Tavares <stephanie.diast@hotmail.com>
2020-03-30 11:31:56 -0300

    finalização do projeto. add opção de deletar

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,5 @@
 let addTarefa = document.querySelector(".addTarefa")
-let container = document.querySelector(".container")
+let container = document.querySelector("main.container")
 let inputTextoTarefa = document.querySelector("#textoTarefa")
 
 // addTarefa.addEventListener('click', function(){
@@ -8,31 +8,51 @@ let inputTextoTarefa = document.querySelector("#textoTarefa")
 // //funcao como parametro da funcao.
 
 
-addTarefa.onclick = function() {
-    
-    let divCard = document.createElement('div');
-    divCard.setAttribute('class','card text-white bg-info m-3');
-
-    let divCardBody = document.createElement('div')
-    divCardBody.setAttribute('class','card-body')
-    
+function addTask() {
     let textoTarefa = inputTextoTarefa.value
 
-    let pConteudo = document.createElement('p')
-    pConteudo.setAttribute('class','card-text')
-    pConteudo.textContent = textoTarefa;
-    
-    let btnAddTarefa = document.createElement('button')
-    btnAddTarefa.setAttribute('class', 'btn btn-light')
-    btnAddTarefa.textContent = "FinalizarTarefa"
+    if (textoTarefa.length > 0) {
+        let divCard = document.createElement('div');
+        divCard.setAttribute('class','card text-white bg-info m-3');
+
+        let divCardBody = document.createElement('div')
+        divCardBody.setAttribute('class','card-body')
+        
+
+        let pConteudo = document.createElement('p')
+        pConteudo.setAttribute('class','card-text')
+        pConteudo.textContent = textoTarefa;
+        
+        let btnEndTarefa = document.createElement('button')
+        btnEndTarefa.setAttribute('class', 'btn btn-light')
+        btnEndTarefa.textContent = "FinalizarTarefa"
+        btnEndTarefa.onclick = function(event) {
+            // let cardRemove = event.target.parentNode.parentNode 
+            // cardRemove.remove()
+            // outra opção de selecionar o elemento. através da relação de pai/filho
+            divCard.remove()
+            //essa é mais direta, uma vez que já criamo 
+        }
+
+        divCard.appendChild(divCardBody)
+        divCardBody.appendChild(pConteudo)
+        divCardBody.appendChild(btnEndTarefa)
+
+        container.appendChild(divCard)
+        inputTextoTarefa.value = ""
+    }   else {
+        alert('Você precisa digitar uma tarefa primeiro.')
+    }
+    }
 
 
-    divCard.appendChild(divCardBody)
-    divCardBody.appendChild(pConteudo)
-    divCardBody.appendChild(btnAddTarefa)
-
-    container.appendChild(divCard)
-    inputTextoTarefa.value = ""
-
-}
+addTarefa.onclick = addTask
 //on click: atribuicao do metodo
+
+
+inputTextoTarefa.onkeyup = function(event) {
+
+    if(event.key === "Enter" ) {
+        addTask()
+}
+}
